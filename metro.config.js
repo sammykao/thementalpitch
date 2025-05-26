33b26d790f6f5de9e0cbab402d607d655844e4b6
Sammy Kao <123334119+sammykao@users.noreply.github.com>
2025-05-26 19:20:12 -0400

    Fix Metro configuration and dependency versions

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('@react-native/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add any custom configurations here
+config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx', 'json'];
+
 module.exports = config;
